1bc1b0e06e79d89f8676b3d335dc50be1220ee57
신수진 <150762691+Shin-Sujin@users.noreply.github.com>
2024-03-16 15:41:00 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    let num = 0;
+    
+    if (Number.isInteger(Math.sqrt(n))){
+        answer = (Math.sqrt(n) + 1)**2;
+    }
+    
+    else{
+        answer = -1;
+    }
+    
+    return answer;
+}
+

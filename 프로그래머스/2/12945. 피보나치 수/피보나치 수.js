8169c7d1df1743bbb837c153f75a8dc97829cdeb
신수진 <150762691+Shin-Sujin@users.noreply.github.com>
2024-03-21 21:51:37 +0900

    [level 2] Title: 피보나치 수, Time: 22.14 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,12 +1,13 @@
 function solution(n) {
-    const mod = 1234567;
-    let fib = [];
-    fib[0] = 0;
-    fib[1] = 1;
+    var answer = 0;
+    let num1 = 0;
+    let num2 = 1;    
     
-    for (let i = 2; i <= n; i++) {
-        fib[i] = (fib[i - 1] + fib[i - 2]) % mod;
+    for(let i = 2; i<=n ; i++){
+        answer = (num1 + num2) % 1234567;
+        num1 = num2;
+        num2 = answer;
     }
-    
-    return fib[n];
+
+    return answer;
 }

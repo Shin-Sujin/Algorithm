67b081c7fab2829c2b5e1e025492268cdd255847
신수진 <150762691+Shin-Sujin@users.noreply.github.com>
2024-03-30 02:00:06 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+function solution(seoul) {
+    var answer = '';
+    let i = 0;
+    while(seoul[i] != 'Kim'){
+        i++;
+        
+    }
+    answer = '김서방은 ' + i + '에 있다';
+    return answer;
+}

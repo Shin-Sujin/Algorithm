d695e2ca0601987ca2b925ff4cd3b93b654d49e3
신수진 <150762691+Shin-Sujin@users.noreply.github.com>
2024-04-08 17:08:52 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,14 @@
+function solution(price) {
+    var answer = 0;
+    if(price >= 500000){
+        return parseInt(price*0.8);
+    }
+    else if(price >= 300000){
+        return parseInt(price*0.9);
+    }else if(price >= 100000){
+        return parseInt(price*0.95);
+    }
+    else{
+        return price;
+    }
+}

c70523bf5f8dfbc6f2fa5e455b0246c6dfd37e47
신수진 <150762691+Shin-Sujin@users.noreply.github.com>
2024-04-29 20:07:49 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    var answer = 0;
+    if(num_list.length>10){
+        for(let i = 0; i<num_list.length ; i++){
+            answer += num_list[i] ;
+        }
+    }else {
+                    answer=1;
+
+        for(let i = 0; i<num_list.length ; i++){
+            answer *=num_list[i] ;
+
+        }
+    }
+    return answer;
+}

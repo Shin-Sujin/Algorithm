fb04985d47d34a22e636c0e698515b33d09356c9
신수진 <150762691+Shin-Sujin@users.noreply.github.com>
2024-10-01 21:51:39 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = [];
+    answer.push(n);
+    while (n != 1){
+        if(n % 2 === 0){
+            n /= 2;
+            answer.push(n);
+        }
+        else if(n % 2 === 1){
+            n = 3 * n + 1;
+            answer.push(n);
+        }
+        
+    }
+    return answer;
+}
